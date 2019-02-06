0d3838872bfe9c83a2adca0450b3e1e32133b11f
matthias <matthias@localhost>
2019-02-06 22:15:12 +0100

    naming...

--- elixir/problem_001.exs
+++ elixir/problem_001.exs
@@ -1,6 +1,6 @@
 defmodule MyList do 
-  def sumOfAllTheNumbersMultiplesOf3Or5BelowX(intNumbersBelow) do 
-    Enum.to_list(1..(intNumbersBelow-1))
+  def sum_of_all_multples_of_3_or_5_below(intBelow) do 
+    Enum.to_list(1..(intBelow-1))
     |> Enum.filter(fn x -> rem(x,3)==0 or rem(x,5)==0 end)
     |> Enum.sum
   end
--- elixir/problem_002.exs
+++ elixir/problem_002.exs
@@ -1,5 +1,5 @@
 defmodule MyList do 
-  def sumOfEvenFibonacciNumbersBelowX(intBelow) do 
+  def sum_of_even_fibonacci_numbers_below(intBelow) do 
     Stream.unfold({0,1}, fn {f1,f2} -> {f1, {f2, f1+f2}} end)
     |> Stream.take_while(&(&1 < intBelow))
     |> Enum.filter(fn x-> rem(x,2)==0 end)
