9e2c6647b847b3a6062644a365bcb808f9295218
matthias <matthias@localhost>
2019-02-07 22:07:52 +0100

    Stream makes more sense here

--- elixir/problem_002.exs
+++ elixir/problem_002.exs
@@ -2,7 +2,7 @@ defmodule MyList do
   def sum_of_even_fibonacci_numbers_below(intBelow) do 
     Stream.unfold({0,1}, fn {f1,f2} -> {f1, {f2, f1+f2}} end)
     |> Stream.take_while(&(&1 < intBelow))
-    |> Enum.filter(fn x-> rem(x,2)==0 end)
+    |> Stream.filter(fn x-> rem(x,2)==0 end)
     |> Enum.sum
   end
 end
