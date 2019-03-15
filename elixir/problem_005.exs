9e133f6fe07850d0374d7247270668d14985b4e6
matthias <matthias@localhost>
2019-03-15 20:44:45 +0100

    kinda slow... but works

--- elixir/problem_005.exs
+++ elixir/problem_005.exs
@@ -1,16 +1,13 @@
-defmodule Squarediff do 
+defmodule Smallestmultiple do 
 
-  def of(numbers) do
-    square_of_sum(numbers) - sum_of_squares(numbers)
-  end
-  defp sum_of_squares(numbers) do
-    numbers
-    |> Stream.map(&(&1*&1))
-    |> Enum.sum()    
+  def evenly_divisible_by(numbers) do
+    Stream.iterate(1, &(&1 + 1))
+    |> Stream.filter(&(divisible_by_all(&1, numbers)))
+    |> Enum.take(1)
+    
   end  
 
-  defp square_of_sum(numbers) do
-    sum = Enum.sum(numbers)
-    sum * sum
+  defp divisible_by_all(number, divisible_by) do
+    !Enum.any?(divisible_by, fn x -> rem(number, x) > 0 end)
   end
 end
